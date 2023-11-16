18163430c77614b488c5febea1728e47ac96d759
georgiouc <103134222+georgiouc@users.noreply.github.com>
2023-11-16 23:33:39 +0200

    Added JavaScript
    Added Jquery functionality, also a few more images and added a few more CSS styling sections

---
+++ jscript/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Submit form and show success alert
+    $("#contact-form").submit(function (event) {
+        event.preventDefault();
+
+        // Your form submission logic goes here
+        // For now, let's just show a simple alert
+        alert("Your form has been submitted.                                                               We will get back to you as soon as possible!");
+    });
+});
